25a9c8e24d8942420fad484ef0e58886d5edd2e4
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-26 00:05:51 +0100

    *

---
+++ CompuFrank/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CompuFrank.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CompuFrank.Areas.Identity.IdentityHostingStartup))]
+namespace CompuFrank.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
