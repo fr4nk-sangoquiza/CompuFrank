c6db0c06f649fb36343d18f99ad17b5c0abf6bde
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-26 18:18:22 +0100

    *

--- CompuFrank/Models/compu_articulos.cs
+++ CompuFrank/Models/compu_articulos.cs
@@ -30,7 +30,7 @@ namespace CompuFrank.Models
         [Display(Name = "Descripción")]
         public string descripcion { get; set; }
 
-        [Display(Name = "URL")]
+        [Display(Name = "URL imagen")]
         public string url_imagen { get; set; }
 
 
