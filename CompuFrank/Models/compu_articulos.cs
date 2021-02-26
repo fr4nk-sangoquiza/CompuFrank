74e9330e4374608c2db14b1b596d194ada6e6287
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-26 01:09:06 +0100

    *

--- CompuFrank/Models/compu_articulos.cs
+++ CompuFrank/Models/compu_articulos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,30 @@ namespace CompuFrank.Models
 {
     public class compu_articulos
     {
+
+        ////////////////////////
+        //    PROPIEDADES
+        ////////////////////////
+        
+        [Key]
+        public int id { get; set; }
+
+        [Display(Name = "Producto")]
+        public string producto { get; set; }
+
+        [Display(Name = "Categoría")]
+        public string categoria { get; set; }
+
+        [Display(Name = "Precio")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal precio { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string descripcion { get; set; }
+
+        [Display(Name = "URL")]
+        public string url_imagen { get; set; }
+
+
     }
 }
