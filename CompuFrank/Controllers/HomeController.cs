68f421629378c3afa492c1c422f74d092768cfa4
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-03-03 22:53:04 +0100

    *

--- CompuFrank/Controllers/HomeController.cs
+++ CompuFrank/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
 
 namespace CompuFrank.Controllers
 {
@@ -49,6 +50,57 @@ namespace CompuFrank.Controllers
         {
             return View();
         }
+
+        public IActionResult contacto()         // MÉTODO GET. Entra a este fichero y me va a retornar la página
+        {                                       // creada Home/contacto.cshtml
+            return View();
+        }
+
+        [HttpPost]                             // MÉTODO POST, para que el 'enviar' pueda subir la información
+       /* public IActionResult Contacto(string NOMBRE, string APELLIDO, string EMAIL, string TELEFONO, string MENSAJE)
+        {
+            enviarCorreo(NOMBRE, APELLIDO, EMAIL, TELEFONO, MENSAJE);
+
+            ViewBag.clave = "¡GRACIAS POR CONTACTAR CON NOSOTROS! En breve nos pondremos en contacto con usted.";
+
+            return View();
+        }*/
+
+                    /* MÉTODO PARA ENVIAR LOS DATOS DEL FORMULARIO */
+      /* public void enviarCorreo(string nombre, string apellido, string email, string telefono, string mensaje)
+        {
+
+            string DireccionOrigen = "fguillermo28sm@gmail.com"; //habrá que configurar nuestro gmail cambiando la seg permitiendo aplicaciones menos seguras
+
+            MailMessage message = new MailMessage(); //Representa un mensaje de correo electrónico que se
+            //puede enviar con la clase SmtpClient.
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            string pass = EMPRESA.Properties.Resources.Password.ToString();
+            smtpClient.Credentials = new System.Net.NetworkCredential() //le pasamos las credenciales
+            {
+                UserName = DireccionOrigen,
+
+                Password = pass
+            };
+
+            //Especifique si el objeto SmtpClient utiliza SSL (Secure Sockets Layer) para cifrar la conexión.
+            smtpClient.EnableSsl = true; //Es true si el objeto SmtpClient utiliza SSL; en caso contrario, es false.
+            //De manera predeterminada, es false.
+            message.From = new MailAddress(DireccionOrigen);
+            message.To.Add(new MailAddress(DireccionOrigen));
+            message.Subject = nombre + "-" + apellido + " - Telefono: " + telefono;
+            message.IsBodyHtml = true;
+            message.Body = mensaje + "Email: " + email;
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network; //Especifica la forma en que se controlarán
+            //los mensajes de correo electrónico salientes.
+            smtpClient.Send(message);
+
+        }*/
+                    /***************************************************************/
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
